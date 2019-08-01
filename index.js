bdef32dc4334c37dacf87b96f009d713410bde78
Sam Cotter <swcotter@comcast.net>
2019-07-31 19:29:54 -0700

    Added folders

--- index.js
+++ index.js
@@ -8,17 +8,6 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
-
-// Star Wars Characters (DATA)
-// =============================================================
-
-
-// Routes
-// =============================================================
-
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
 
